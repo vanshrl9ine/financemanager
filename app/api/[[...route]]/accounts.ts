15037a8cb3fb0c461f3dddda2df09eee6a74f7b4
vanshrl9ine <guptavansh67@gmail.com>
2024-08-09 20:58:57 +0530

    get-accounts hook and api  working

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -2,17 +2,23 @@ import { Hono } from "hono";
 import { db } from "../../../db/drizzle";
 import { accounts } from "../../../db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import {HTTPException} from 'hono/http-exception';
+import { eq } from "drizzle-orm";
 const app=new Hono()
 .get('/',clerkMiddleware(),async(c)=>{
     const auth=getAuth(c);
     if(!auth?.userId){
-        return c.json({error:"Unauthorized"},401);
-    }
+        throw new HTTPException(401,{
+            res:c.json({error:"unauthorized"},401),
+        });
+    };
     const data=await db.select({
         id:accounts.id,
         name:accounts.name,
     })
-    .from(accounts);
+    .from(accounts)
+    .where(eq(accounts.userId,auth.userId));
+    
     return c.json({data});
 });
 export default app;
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -5,8 +5,14 @@ import {zValidator} from '@hono/zod-validator';
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 export const runtime = 'edge'
 import accounts from './accounts';
+import { HTTPException } from 'hono/http-exception';
 const app = new Hono().basePath('/api')
-
+app.onError((err,c)=>{
+    if(err instanceof HTTPException){
+        return err.getResponse();
+    }
+    return c.json({error:"internal error"});
+})
 const routes=app
 .route("/accounts",accounts);
 
