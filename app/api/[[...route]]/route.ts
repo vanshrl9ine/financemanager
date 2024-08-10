775add545531107f3ecd6a1ba7cb0504733a73cd
vanshrl9ine <guptavansh67@gmail.com>
2024-08-11 01:05:21 +0530

    everything works fine accounts categories

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -7,12 +7,14 @@ export const runtime = 'edge'
 import accounts from './accounts';
 import categories from './categories'
 import { HTTPException } from 'hono/http-exception';
+import  transactions  from './transactions'
 
 const app = new Hono().basePath('/api')
 
 const routes=app
 .route("/accounts",accounts)
-.route("/categories",categories);
+.route("/categories",categories)
+.route("/transactions",transactions);
 export const GET = handle(app)
 export const POST = handle(app)
 export const PATCH = handle(app)
