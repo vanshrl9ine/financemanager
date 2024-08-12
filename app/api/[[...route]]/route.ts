3f3e52cc4066709a706b0ee50dd6101bc674ba6e
vanshrl9ine <guptavansh67@gmail.com>
2024-08-12 14:43:27 +0530

    summary.ts

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -8,13 +8,15 @@ import accounts from './accounts';
 import categories from './categories'
 import { HTTPException } from 'hono/http-exception';
 import  transactions  from './transactions'
-
+import summary from './summary'
 const app = new Hono().basePath('/api')
 
 const routes=app
 .route("/accounts",accounts)
 .route("/categories",categories)
-.route("/transactions",transactions);
+.route("/transactions",transactions)
+.route("/summary",summary)
+
 export const GET = handle(app)
 export const POST = handle(app)
 export const PATCH = handle(app)
